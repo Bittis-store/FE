8b1b6acf35a135859b6e250934e1ceabaf7e9d90
chinhnxph45367 <chinhnxpg45367@fpt.edu.vn>
2025-03-13 23:58:18 +0700

    feat: trang tài khoản

--- src/pages/client/Account/Profile/Profile.tsx
+++ src/pages/client/Account/Profile/Profile.tsx
@@ -65,6 +65,117 @@ const Profile = () => {
         avatar?: IProductFiles;
     };
 
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        const formDataUpdateProfile = new FormData();
+
+        formDataUpdateProfile.append('name', values.name);
+        formDataUpdateProfile.append('email', values.email);
+        formDataUpdateProfile.append('phone', values.phone);
+        formDataUpdateProfile.append('avatar', (values.avatar?.fileList[0] as IThumbnailAntd)?.originFileObj);
+
+        updateProfile(formDataUpdateProfile);
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const thumbnailValidator = async (_: any, thumbnail: IProductFiles) => {
+        if (
+            thumbnail &&
+            thumbnail.fileList &&
+            thumbnail.fileList.length > 0 &&
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (thumbnail.fileList[0] as any).originFileObj
+        ) {
+            if (thumbnail?.fileList?.length < 1 || !thumbnail) {
+                return errorMessage('Please input your thumbnail!');
+            }
+            if (thumbnail && thumbnail.file.size && thumbnail?.file.size >= MAX_SIZE) {
+                return errorMessage('Image size must be smaller than 5MB!');
+            }
+            if (thumbnail?.file.type && !ACCEPT_FILE_TYPE.includes(thumbnail?.file.type)) {
+                return errorMessage('Only accept png, jpg and jpeg type!');
+            }
+        }
+        return Promise.resolve();
+    };
+
+    useEffect(() => {
+        if (profile) {
+            form.setFieldsValue({
+                name: profile.name,
+                phone: profile.phone,
+                email: profile.email,
+            });
+        }
+    }, [profile, form]);
+
+    useEffect(() => {
+        if (profile) {
+            const thumbnailConvert = convertApiResponseToFileList({
+                url: profile?.avatar,
+                urlRef: profile?.avatar,
+                isArr: true,
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            }) as UploadFile<any>[];
+
+            setThumbnailFile(thumbnailConvert);
+        }
+    }, [profile]);
+
+    const uploadButton = (
+        <button style={{ border: 0, background: 'none' }} type='button'>
+            {loading ? <LoadingOutlined /> : <PlusOutlined />}
+            <div style={{ marginTop: 8 }}>Upload</div>
+        </button>
+    );
+
+    const handleChangeThumbnail: UploadProps['onChange'] = ({ fileList: newFileList }) => setThumbnailFile(newFileList);
+
+    const handlePreview = async (file: UploadFile) => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj as FileType);
+        }
+
+        setPreviewThumbnail(file.url || (file.preview as string));
+        setPreviewThumbnailOpen(true);
+    };
+
+    const customItemRender: UploadProps['itemRender'] = (originNode, file, fileList, actions) => {
+        return (
+            <div className='ant-upload-list-item ant-upload-list-item-undefined'>
+                <img className='' src={file.thumbUrl || file.url} alt={file.name} />
+                <span className='ant-upload-list-item-actions'>
+                    <span
+                        onClick={actions.preview}
+                        className='ant-btn css-dev-only-do-not-override-mzwlov ant-btn-text ant-btn-sm ant-btn-icon-only ant-upload-list-item-action text-white'
+                    >
+                        <EyeOutlined />
+                    </span>
+                    <span
+                        onClick={actions.remove}
+                        className='ant-btn css-dev-only-do-not-override-mzwlov ant-btn-text ant-btn-sm ant-btn-icon-only ant-upload-list-item-action text-white'
+                    >
+                        <DeleteOutlined />
+                    </span>
+                </span>
+            </div>
+        );
+    };
+
+    const handleChangePassword: FormProps<ChangePassword>['onFinish'] = (values) => {
+        changePassword(
+            {
+                password: values.password,
+                newPassword: values.newPassword,
+            },
+            {
+                onSuccess() {
+                    handleCancel();
+                },
+            }
+        );
+        form.resetFields();
+    };
+
     return <></>;
 };
 
