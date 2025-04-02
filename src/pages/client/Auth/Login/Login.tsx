0e0bf1dfa84819125da94e729556605fe3c41c68
vanhdeptraivcl <vietcongtu66@gmail.com>
2025-04-02 17:12:40 +0700

    refactor: login UI

--- src/pages/client/Auth/Login/Login.tsx
+++ src/pages/client/Auth/Login/Login.tsx
@@ -2,29 +2,18 @@ import SignImg from '~/assets/img/male-shoes.jpg';
 import { MAIN_ROUTES } from '~/constants/router';
 import useDocumentTitle from '~/hooks/_common/useDocumentTitle';
 import { useAuthLogin } from '~/hooks/Auth/Mutation/useAuthLogin';
-import { LoginFormData, loginSchema } from '~/validation/Auth/Auth';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, Input, Spin } from 'antd';
-import { Controller, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
     useDocumentTitle('BITIS - Đăng nhập');
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-            email: '',
-            password: '',
-        },
-    });
+    const [form] = Form.useForm();
     const { mutate, isPending } = useAuthLogin();
-    const onSubmit = (data: LoginFormData) => {
-        mutate(data);
+
+    const onFinish = (values: { email: string; password: string }) => {
+        mutate(values);
     };
+
     return (
         <div className='md:max-w-standard mx-auto mt-12 w-full xl:max-w-7xl'>
             <div className='flex justify-between'>
@@ -34,47 +23,45 @@ const Login = () => {
                 <div className='flex basis-[40%] flex-col items-center justify-center gap-10'>
                     <h1 className='font-inter text-4xl font-medium'>Đăng nhập</h1>
                     <p>Chào mừng bạn đến với BITIS</p>
-                    <Form onFinish={handleSubmit(onSubmit)} className='flex w-full flex-col' layout='vertical'>
+                    <Form form={form} onFinish={onFinish} layout='vertical' className='flex w-full flex-col'>
                         <Form.Item
+                            name='email'
                             label='Email'
-                            validateStatus={errors.email ? 'error' : ''}
-                            help={errors.email?.message}
+                            rules={[
+                                { required: true, message: 'Vui lòng nhập email' },
+                                { type: 'email', message: 'Vui lòng nhập email hợp lệ' },
+                            ]}
                         >
-                            <Controller
-                                name='email'
-                                control={control}
-                                render={({ field }) => (
-                                    <Input {...field} className='h-[48px]' placeholder='Địa chỉ email' />
-                                )}
-                            />
+                            <Input className='h-[48px]' placeholder='Địa chỉ email' />
                         </Form.Item>
                         <Form.Item
+                            name='password'
                             label='Mật khẩu'
-                            validateStatus={errors.password ? 'error' : ''}
-                            help={errors.password?.message}
+                            rules={[
+                                { required: true, message: 'Vui lòng nhập mật khẩu' },
+                                { min: 6, message: 'Mật khẩu phải có ít nhất 6 ký tự' },
+                            ]}
                         >
-                            <Controller
-                                name='password'
-                                control={control}
-                                render={({ field }) => (
-                                    <Input.Password {...field} className='h-[48px]' placeholder='Mật khẩu' />
-                                )}
-                            />
+                            <Input.Password className='h-[48px]' placeholder='Mật khẩu' />
                         </Form.Item>
 
-                        <button
-                            disabled={isPending}
-                            type='submit'
-                            className='bg-global hover:bg-primary mt-4 h-[48px] w-full cursor-pointer rounded-md font-medium text-white duration-300'
-                        >
-                            {isPending ? <Spin className='text-white' /> : 'Đăng nhập'}
-                        </button>
-                        <Link
-                            to={MAIN_ROUTES.FORGOT_PASSWORD}
-                            className='text-global hover:text-primary mt-4 text-center duration-300'
-                        >
-                            Quên mật khẩu?
-                        </Link>
+                        <Form.Item>
+                            <button
+                                disabled={isPending}
+                                type='submit'
+                                className='bg-global hover:bg-primary mt-4 h-[48px] w-full cursor-pointer rounded-md font-medium text-white duration-300'
+                            >
+                                {isPending ? <Spin className='text-white' /> : 'Đăng nhập'}
+                            </button>
+                        </Form.Item>
+                        <div className='text-center'>
+                            <Link
+                                to={MAIN_ROUTES.FORGOT_PASSWORD}
+                                className='text-global hover:text-primary duration-300'
+                            >
+                                Quên mật khẩu?
+                            </Link>
+                        </div>
                     </Form>
                 </div>
             </div>
