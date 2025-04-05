f7469f34f6086fe82f23ab4ae596599a99993c26
chinhnxph45367 <chinhnxph45367@fpt.edu.vn>
2025-04-05 12:50:35 +0700

    fix: list product

--- src/pages/Admin/_product_/Helper/handleEditProduct.ts
+++ src/pages/Admin/_product_/Helper/handleEditProduct.ts
@@ -1,34 +1,34 @@
 import { IProductForm } from '~/types/Product';
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
-
-const renameFile = (file: File, uuidString:string,newName: string): File => {
-
+const renameFile = (file: File, uuidString: string, newName: string): File => {
     return new File([file], `${uuidString}_____${newName}_____.${newName.split('.').pop()}`, {
-      type: file.type,
-      lastModified: file.lastModified,
+        type: file.type,
+        lastModified: file.lastModified,
     });
-  };
+};
 export const handleEditProduct = (
     data: IProductForm,
     id: string,
-    updateProduct: ({ data, id }: { data: FormData; id: string }) => void,
+    updateProduct: ({ data, id }: { data: FormData; id: string }) => void
 ) => {
     const formData = new FormData();
-    const { name, description, variants, price, discount, tags, category } =
-        data;
+    const { name, description, variants, price, discount, tags, category } = data;
     const firstElement = 0;
     const newVariants = [];
 
     if (variants) {
         for (const [, value] of Object.entries(variants)) {
             if (value?.thumbnail?.fileList?.[firstElement]?.originFileObj) {
-                const uuidString =uuidv4()
+                const uuidString = uuidv4();
 
                 formData.append(
-                    'variantImages',renameFile(
-                    value.thumbnail?.fileList?.[firstElement]
-                        .originFileObj as File, uuidString,value.thumbnail?.fileList[firstElement].name),
+                    'variantImages',
+                    renameFile(
+                        value.thumbnail?.fileList?.[firstElement].originFileObj as File,
+                        uuidString,
+                        value.thumbnail?.fileList[firstElement].name
+                    )
                 );
                 Object.assign(value, {
                     imageUrlRef: `${uuidString}_____${value.thumbnail?.fileList[firstElement].name}_____.${value.thumbnail?.fileList[firstElement].name.split('.').pop()}`,
@@ -59,7 +59,7 @@ export const handleEditProduct = (
             }
         }
     }
-    /* eslint-enable */
+
     formData.append('name', name);
     formData.append('category', category);
     formData.append('price', `${price}`);
--- src/pages/Admin/_product_/Helper/tableList.tsx
+++ src/pages/Admin/_product_/Helper/tableList.tsx
@@ -1,7 +1,8 @@
-import { EditOutlined, EllipsisOutlined } from '@ant-design/icons';
-import { Button, Popconfirm, Space, TableProps, Tag, Tooltip } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+import { Popconfirm, Space, TableProps, Tooltip } from 'antd';
 import { ColumnType } from 'antd/es/table';
 import { Link } from 'react-router-dom';
+import { useMemo } from 'react';
 import { Params } from '~/types/Api';
 import { Currency } from '~/utils';
 import { IProduct } from '~/types/ProductNew';
@@ -10,6 +11,12 @@ interface IFilter {
     text: string;
     value: string;
 }
+
+const ActionLink = ({ onClick, children }: { onClick?: () => void; children: React.ReactNode }) => (
+    <p className='cursor-pointer text-blue-500 transition-colors duration-500 hover:text-blue-400' onClick={onClick}>
+        {children}
+    </p>
+);
 
 export const ProductsListColumns = ({
     categoriesFilter,
@@ -28,158 +35,151 @@ export const ProductsListColumns = ({
     getColumnSearchProps: (dataIndex: string) => ColumnType<any>;
     getFilteredValue: (key: string) => string[] | undefined;
 }): TableProps<IProduct>['columns'] => {
-    return [
-        {
-            title: 'Tên sản phẩm',
-            dataIndex: 'name',
-            key: 'search',
-            width: '30%',
-            ...getColumnSearchProps('name'),
-            render: (text, record) => (
-                <>
-                    <div className='flex items-center gap-2'>
-                        <div>
-                            <h4 className='max-w-[300px] truncate text-[16px] font-semibold'>{text}</h4>
-                            <p className='text-[10px]'>ID: {record._id}</p>
-                        </div>
-                    </div>
-                    <div className='ms-7 mt-1 p-5'>
-                        {record.variants.map((item, index) => (
-                            <div className='my-4 flex items-center gap-2' key={index}>
-                                <div>
-                                    <img src={item?.image} className='h-8 w-8 object-cover' alt={record.name + index} />
-                                </div>
-                                <div>
-                                    <p className='text-[10px]'>Kích thước: {item?.size.name}</p>
-                                    <p className='text-[10px]'>Màu sắc: {item?.color.name}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            ),
-        },
-        {
-            title: 'Đã bán',
-            key: 'sold',
-            render: (_, record) => <p className='text-center'>{record.sold}</p>,
-            responsive: ['md'],
-        },
-        {
-            title: 'Giá tiền (VNĐ)',
-            key: 'price',
-            render: (_, record) => {
-                return <>{Currency.format(record.price)}</>;
-            },
-        },
-        {
-            title: 'Kho hàng',
-            key: 'stock',
-            render: (_, record) => (
-                <>
-                    <div className='flex flex-col justify-between'>
-                        <p className='h-14 whitespace-nowrap'>
-                            Tổng:{' '}
-                            {record?.variants.reduce((acc, curr) => acc + (curr?.stock || 0), 0) !== 0 ? (
-                                record?.variants.reduce((acc, curr) => acc + (curr?.stock || 0), 0)
-                            ) : (
-                                <span className='text-red-500'>Hết hàng</span>
-                            )}
-                        </p>
-                    </div>
-                    <div className=''>
-                        {record.variants.map((item, index) => (
-                            <p className='my-4 h-8' key={index}>
-                                {item?.stock ? item.stock : <span className='text-red-500'>Hết hàng</span>}
-                            </p>
-                        ))}
-                    </div>
-                </>
-            ),
-        },
-        {
-            title: 'Thẻ hàng',
-            key: 'tags',
-            filters: tagsFilter,
-            filteredValue: getFilteredValue('tags'),
-            render: (_, record) => {
-                return <h4>{record.tags.map((item: any) => item.name).join(', ')}</h4>;
-            },
-        },
-        {
-            title: 'Danh mục',
-            key: 'category',
-            filters: categoriesFilter,
-            filteredValue: getFilteredValue('category'),
-            render: (_, record) => {
-                return <h4>{typeof record.category === 'object' ? record.category.name : record.category}</h4>;
-            },
-        },
-        {
-            title: 'Trạng thái',
-            key: 'isActive',
-            filteredValue: getFilteredValue('isActive'),
-            filters: [
-                { text: 'Ẩn', value: 'false' },
-                { text: 'Hiện', value: 'true' },
-            ],
-            render: (_, record) => {
-                return (
+    return useMemo(
+        () => [
+            {
+                title: 'Tên sản phẩm',
+                dataIndex: 'name',
+                key: 'search',
+                width: '30%',
+                ...getColumnSearchProps('name'),
+                render: (text, record) => (
                     <>
-                        <p className='text-red-500'>{!record.isActive && 'Đã ẩn'}</p>
-                        <p className='text-green-400'>{record.isActive && 'Đang hiển thị'}</p>
+                        <div className='flex items-center gap-2'>
+                            <div>
+                                <h4 className='max-w-[300px] truncate text-[16px] font-semibold'>{text}</h4>
+                                <p className='text-[10px]'>ID: {record._id}</p>
+                            </div>
+                        </div>
+                        <div className='ms-7 mt-1 p-5'>
+                            {record.variants.map((item, index) => (
+                                <div className='my-4 flex items-center gap-2' key={index}>
+                                    <div>
+                                        <img
+                                            src={item?.image}
+                                            className='h-8 w-8 object-cover'
+                                            alt={`${record.name}-${index}`}
+                                        />
+                                    </div>
+                                    <div>
+                                        <p className='text-[10px]'>Kích thước: {item?.size.name}</p>
+                                        <p className='text-[10px]'>Màu sắc: {item?.color.name}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </>
-                );
+                ),
             },
-        },
-
-        {
-            title: 'Thao tác',
-            key: 'action',
-            render: (_, record) => (
-                <Space key={record._id} className='flex flex-col items-start justify-start'>
-                    <Tooltip title='Cập nhật'>
-                        <Link
-                            to={`/admin/products/${record._id}/edit`}
-                            className='text-blue-500 transition-colors duration-500 hover:text-blue-400'
-                        >
-                            Cập nhật
-                        </Link>
-                    </Tooltip>
-                    {record.isActive && (
-                        <Tooltip title='Ẩn sản phẩm này'>
-                            <Popconfirm
-                                placement='leftBottom'
-                                title='Ấn sản phẩm khỏi người dùng?'
-                                description='Người dùng sẽ không thể thấy sản phẩm này của bạn.'
-                                onConfirm={() => mutateHideProduct(record._id)}
-                                okText='Đồng ý'
-                                cancelText='Đóng'
-                            >
-                                <p className='cursor-pointer text-blue-500 transition-colors duration-500 hover:text-blue-400'>
-                                    Ẩn đi
+            {
+                title: 'Đã bán',
+                key: 'sold',
+                render: (_, record) => <p className='text-center'>{record.sold}</p>,
+                responsive: ['md'],
+            },
+            {
+                title: 'Giá tiền (VNĐ)',
+                key: 'price',
+                render: (_, record) => <>{Currency.format(record.price)}</>,
+            },
+            {
+                title: 'Kho hàng',
+                key: 'stock',
+                render: (_, record) => {
+                    const totalStock = record?.variants.reduce((acc, curr) => acc + (curr?.stock || 0), 0);
+                    return (
+                        <>
+                            <div className='flex flex-col justify-between'>
+                                <p className='h-14 whitespace-nowrap'>
+                                    Tổng: {totalStock > 0 ? totalStock : <span className='text-red-500'>Hết hàng</span>}
                                 </p>
-                            </Popconfirm>
-                        </Tooltip>
-                    )}
-                    {!record.isActive && (
-                        <Tooltip title='Hiện thị sản phẩm này'>
-                            <Popconfirm
-                                placement='leftBottom'
-                                title='Hiện thị sản phẩm này?'
-                                description='Người dùng sẽ thầy sản phẩm này của bạn.'
-                                onConfirm={() => mutateShowProduct(record._id)}
-                                okText='Đồng ý'
-                                cancelText='Đóng'
+                            </div>
+                            <div className=''>
+                                {record.variants.map((item, index) => (
+                                    <p className='my-4 h-8' key={index}>
+                                        {item?.stock ? item.stock : <span className='text-red-500'>Hết hàng</span>}
+                                    </p>
+                                ))}
+                            </div>
+                        </>
+                    );
+                },
+            },
+            {
+                title: 'Thẻ hàng',
+                key: 'tags',
+                filters: tagsFilter,
+                filteredValue: getFilteredValue('tags'),
+                render: (_, record) => <h4>{record.tags.map((item: any) => item.name).join(', ')}</h4>,
+            },
+            {
+                title: 'Danh mục',
+                key: 'category',
+                filters: categoriesFilter,
+                filteredValue: getFilteredValue('category'),
+                render: (_, record) => (
+                    <h4>{typeof record.category === 'object' ? record.category.name : record.category}</h4>
+                ),
+            },
+            {
+                title: 'Trạng thái',
+                key: 'isActive',
+                filteredValue: getFilteredValue('isActive'),
+                filters: [
+                    { text: 'Ẩn', value: 'false' },
+                    { text: 'Hiện', value: 'true' },
+                ],
+                render: (_, record) => (
+                    <>
+                        {!record.isActive && <p className='text-red-500'>Đã ẩn</p>}
+                        {record.isActive && <p className='text-green-400'>Đang hiển thị</p>}
+                    </>
+                ),
+            },
+            {
+                title: 'Thao tác',
+                key: 'action',
+                render: (_, record) => (
+                    <Space key={record._id} className='flex flex-col items-start justify-start'>
+                        <Tooltip title='Cập nhật'>
+                            <Link
+                                to={`/admin/products/${record._id}/edit`}
+                                className='text-blue-500 transition-colors duration-500 hover:text-blue-400'
                             >
-                                <p className='cursor-pointer text-blue-500 transition-colors duration-500 hover:text-blue-400'>
-                                    Hiển thị
-                                </p>
-                            </Popconfirm>
+                                Cập nhật
+                            </Link>
                         </Tooltip>
-                    )}
-                </Space>
-            ),
-        },
-    ];
+                        {record.isActive ? (
+                            <Tooltip title='Ẩn sản phẩm này'>
+                                <Popconfirm
+                                    placement='leftBottom'
+                                    title='Ấn sản phẩm khỏi người dùng?'
+                                    description='Người dùng sẽ không thể thấy sản phẩm này của bạn.'
+                                    onConfirm={() => mutateHideProduct(record._id)}
+                                    okText='Đồng ý'
+                                    cancelText='Đóng'
+                                >
+                                    <ActionLink>Ẩn đi</ActionLink>
+                                </Popconfirm>
+                            </Tooltip>
+                        ) : (
+                            <Tooltip title='Hiện thị sản phẩm này'>
+                                <Popconfirm
+                                    placement='leftBottom'
+                                    title='Hiện thị sản phẩm này?'
+                                    description='Người dùng sẽ thầy sản phẩm này của bạn.'
+                                    onConfirm={() => mutateShowProduct(record._id)}
+                                    okText='Đồng ý'
+                                    cancelText='Đóng'
+                                >
+                                    <ActionLink>Hiển thị</ActionLink>
+                                </Popconfirm>
+                            </Tooltip>
+                        )}
+                    </Space>
+                ),
+            },
+        ],
+        [categoriesFilter, tagsFilter, getColumnSearchProps, getFilteredValue, mutateHideProduct, mutateShowProduct]
+    );
 };
